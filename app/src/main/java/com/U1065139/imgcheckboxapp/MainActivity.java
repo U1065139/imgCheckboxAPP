69cbadbc33b973a2dbdb9e6bed1d69d365459583
U1065139 <lance880810@gmail.com>
2020-04-13 12:21:49 +0800

    first commit

---
+++ app/src/main/java/com/U1065139/imgcheckboxapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.U1065139.imgcheckboxapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
